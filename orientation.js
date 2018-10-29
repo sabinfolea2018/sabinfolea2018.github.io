460ff65b9eb204269dc5c1df744f8c238c430c17
sabinfolea2018 <43954558+sabinfolea2018@users.noreply.github.com>
2018-10-29 12:50:35 +0200

    d

--- orientation.js
+++ orientation.js
@@ -5,4 +5,12 @@
 	 document.getElementById("id_alpha").innerHTML = e.alpha;
 	 document.getElementById("id_beta").innerHTML = e.beta;
 	 document.getElementById("id_gamma").innerHTML = e.gamma;
+	 
+	 var canvas = document.getElementById("id_canvas");
+	 var context = canvas.getContext("2d");
+	 context.clearRect(0,0, canvas.width, canvas.height);
+	 var R=10;
+	 context.beginPath();
+	 context.arc(canvas.width/2 + e.gamma/90 * (canvas.width/2 - R), canvas.height/2 + e.beta/90 * (canvas.height/2 - R), R, 0, 2* Math.PI);
+	 context.stroke();
  }
