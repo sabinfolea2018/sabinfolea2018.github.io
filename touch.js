166b3360c79dd65010c1d8dc800ceec3e81dc947
sabinfolea2018 <43954558+sabinfolea2018@users.noreply.github.com>
2018-11-26 11:40:14 +0200

    3

--- touch.js
+++ touch.js
@@ -5,7 +5,7 @@
   canvas.addEventListener("touchmove", on_touch_move);
   
   var canvas_bounding_rect = canvas.getBoundingClientRect();
-  
+  var last_pos = {x: 0; y: 0};
   
   function on_touch_start(e) 
   {
@@ -18,17 +18,23 @@
 					0,
 					2 * Math.PI);
 		context.stroke();
+		last_pos.x = e.changeTouches[i].pageX;
+		last_pos.y = e.changeTouches[i].pageY;
 	  }
   }
   
   
   function on_touch_move(e) 
   {
+	  e.prevetDefault();
 	  for (var i = 0; i<e.changeTouches.length; i++) {
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
+		context.lineTo(e.changeTouches[i].pageX - canvas_bounding_rect.left, e.changeTouches[i].pageY - canvas_bounding_rect.top);
+		
 		context.arc(e.changeTouches[i].pageX - canvas_bounding_rect.left,
-					e.changeTouches[i].pageX - canvas_bounding_rect.top,
+					e.changeTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0,
 					2 * Math.PI);
