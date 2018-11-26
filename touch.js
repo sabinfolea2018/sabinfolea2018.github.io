b97be390698ea4b4bc501d89e38f5b5acd5d83c9
sabinfolea2018 <43954558+sabinfolea2018@users.noreply.github.com>
2018-11-26 11:47:32 +0200

    line

--- touch.js
+++ touch.js
@@ -30,14 +30,17 @@
 	  for (var i = 0; i<e.changeTouches.length; i++) {
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.lineWidth = 20;
 		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
 		context.lineTo(e.changeTouches[i].pageX - canvas_bounding_rect.left, e.changeTouches[i].pageY - canvas_bounding_rect.top);
-		
+		context.lineWidth = 1;
 		context.arc(e.changeTouches[i].pageX - canvas_bounding_rect.left,
 					e.changeTouches[i].pageY - canvas_bounding_rect.top,
 					10,
 					0,
 					2 * Math.PI);
 		context.stroke();
+		last_pos.x = e.changeTouches[i].pageX;
+		last_pos.y = e.changeTouches[i].pageY;
 	  }
   }
