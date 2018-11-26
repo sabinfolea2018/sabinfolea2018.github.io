d763bd6bd5974fe1ed85f3c089424e247a959b57
sabinfolea2018 <43954558+sabinfolea2018@users.noreply.github.com>
2018-11-26 11:24:17 +0200

    1

--- touch.js
+++ touch.js
@@ -1,15 +1,17 @@
-  document.getElementById("id_bussiness_version").innerHTML = "Business version: 2018.11.26.0";
+  document.getElementById("id_bussiness_version").innerHTML = "Business version: 2018.11.26.1";
   
   var canvas = document.getElementById("id_canvas");
   canvas.addEventListener("touchstart", on_touch_start);
+  
+  var canvas_bounding_rect = canvas.getBoundingClientRect();
   
   function on_touch_start(e) 
   {
 	  for (var i = 0; i<e.changeTouches.length; i++) {
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changeTouches[i].pageX,
-					e.changeTouches[i].pageX,
+		context.arc(e.changeTouches[i].pageX - canvas_bounding_rect.left,
+					e.changeTouches[i].pageX - canvas_bounding_rect.top,
 					10,
 					0,
 					2 * Math.PI);
