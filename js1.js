9d59c5c0776dd24bfe46b7b952a36901c685c22b
sabinfolea2018 <43954558+sabinfolea2018@users.noreply.github.com>
2019-01-09 19:12:32 +0200

    Add files via upload

--- js1.js
+++ js1.js
@@ -21,3 +21,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
 });
+let button = document.getElementById("get-location");
+let latText = document.getElementById("latitude");
+let longText = document.getElementById("longitude");
+
+button.addEventListener("click", function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+    latText.innerText = lat.toFixed(2);
+    longText.innerText = long.toFixed(2);
+  });
+});
