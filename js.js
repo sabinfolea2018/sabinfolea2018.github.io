9dc5eb47871418e32fdcf6399d68e01b6694ab43
sabinfolea2018 <43954558+sabinfolea2018@users.noreply.github.com>
2019-01-09 18:54:40 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -2,22 +2,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', function(eventData) {
-    var id_gama = eventData.gamma;
-    var id_beta = eventData.beta;
-    var id_alpha = eventData.alpha;
-    deviceOrientationHandler(id_gama, id_beta, id_alpha);
-  }, false);
-} else {
-};
+    var tiltLR = eventData.gamma;
+    var tiltFB = eventData.beta;
+    var dir = eventData.alpha
 
-    function deviceOrientationHandler(id_gama, id_beta, id_alpha) {
-      document.getElementById("id_gama").innerHTML = Math.ceil(gamma);
-      document.getElementById("id_beta").innerHTML = Math.ceil(beta);
-      document.getElementById("id_alpha").innerHTML = Math.ceil(alpha);
+    deviceOrientationHandler(tiltLR, tiltFB, dir);
+  }, false);
+} else { };
+
+    function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+      document.getElementById("tiltLR").innerHTML = Math.ceil(tiltLR);
+      document.getElementById("tiltFB").innerHTML = Math.ceil(tiltFB);
+      document.getElementById("direction").innerHTML = Math.ceil(dir);
       var compassDisc = document.getElementById("compassDiscImg");
-      compassDisc.style.webkitTransform = "rotate("+ id_alpha +"deg)";
-      compassDisc.style.MozTransform = "rotate("+ id_alpha +"deg)";
-      compassDisc.style.transform = "rotate("+ id_alpha +"deg)";
+      compassDisc.style.webkitTransform = "rotate("+ dir +"deg)";
+      compassDisc.style.MozTransform = "rotate("+ dir +"deg)";
+      compassDisc.style.transform = "rotate("+ dir +"deg)";
     }
 
 });
